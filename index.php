6982eac6ee23125b8c83dff652f21efb87e080ca
elie <elie.verholle@gmail.com>
2019-05-19 14:27:40 +0200

    modif

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-    <title>Galerie d'Image</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="style.css"/>
 </head>
 <body>
@@ -15,9 +14,9 @@
     <div class="col-md-4 col-md-offset-4">
         <form action="upload.php" method="post" enctype="multipart/form-data">
             <fieldset>
-                <legend>Ajoutez vos fichiers</legend>
+                <legend>Ajoutez vos Images</legend>
                 <div class="form-group">
-                    <label for="file">Ajoutez vos fichiers (taille max. 1Mo):</label>
+                    <label for="file">Ajoutez vos Images (taille max. 1Mo):</label>
                     <input type="hidden" name="MAX_FILE_SIZE" value="1000000"/>
                     <input type="file" name="files[]" id="file" multiple/>
                 </div>
